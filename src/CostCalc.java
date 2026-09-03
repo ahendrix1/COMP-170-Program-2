fc3c421629f730a03cdb8a9fa8b874294e525599
Avery Hendrix <ahendrix1@luc.edu>
2026-09-03 18:49:32 -0500

    Now im finished

--- src/CostCalc.java
+++ src/CostCalc.java
@@ -47,11 +47,12 @@ public class CostCalc {
         final int WGTLRG = 31;
         final int WFTXXL = 81;
 
-        final double DISCNT = 0.02;
-
         String name, breed, group;
+
         int age, weight, days;
-        Double cost = 0.0;
+        int cost = 0;
+
+        Double total = 0.0;
 
         // INSTANTIATE SCANNER OBJECT
         Scanner sc = new Scanner(System.in);
@@ -77,24 +78,27 @@ public class CostCalc {
         System.out.print("Enter number of days: ");
         days = sc.nextInt();
         // LOGIC TO DETERMINE ESTIMATED COST BASED ON WEIGHT
-        if (weight > WGTSML) {
-            cost = (double) FEESML;
-        } else if (weight > WGTMID) {
-            cost = (double) FEEMID;
+        if (weight > WFTXXL) {
+            cost = FEEXXL;
         } else if (weight > WGTLRG) {
-            cost = (double) FEELRG;
-        } else if (weight > WFTXXL) {
-            cost = (double) FEEXXL;
+            cost = FEELRG;
+        } else if (weight > WGTMID) {
+            cost = FEEMID;
+        } else if (weight > WGTSML) {
+            cost = FEESML;
         }
-
         // DETERMINE ESTIMATED COST BASED ON WEIGHT AND NUMBER OF DAYS
         cost = cost * days;
+        System.out.println(cost);
         // DETERMINE IF ESTIMATED COST RECEIVE A DISCOUNT
+
         if (cost > 165) {
-            cost = cost - (cost * DISCNT);
+            total = cost * 0.98;
         }
+
+        System.out.println(cost);
+        System.out.println(total);
         // DETERMINE DOGS BOARDING GROUP BASED ON AGE
-        //
         switch (age) {
             case 0:
                 group = "BLUE";
@@ -117,14 +121,17 @@ public class CostCalc {
         System.out.println(" \n ====================== \n SUMMARY \n ======================");
         System.out.println("NAME: " + name);
         System.out.println("BREED: " + breed);
-        System.out.println("DAYS: " + days);
         System.out.println("WEIGHT: " + weight);
+        System.out.println("AGE: " + age);
+        System.out.println("DAYS: " + days);
         System.out.println("GROUP: " + group);
 
-        if (cost % 5 != 0) {
+        if (total != 0.0) {
             System.out.println("\n You qualify for a discount.");
+        } else {
+            total = (double) cost;
         }
-        System.out.println("COST: " + cost);
+        System.out.println("COST: " + total);
 
         sc.close();
 
